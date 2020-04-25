c9a8d42d074d85453116f7f351359254aa52edc7
Leo Linbeck III <leo3@linbeck.com>
2020-04-25 17:32:39 -0500

    Recompile

--- production/brevitest_v1_0/src/brevitest_v1_0.cpp
+++ production/brevitest_v1_0/src/brevitest_v1_0.cpp
@@ -1638,7 +1638,6 @@ int particle_command(String arg)
             result = stage_position;
             break;
         case 4: // read optical sensors (param) after moving to read position
-            stop_temperature_control();
             wake_motor();
             move_stage_to_optical_read_position();
             sleep_motor();
@@ -1649,7 +1648,6 @@ int particle_command(String arg)
             read_optical_sensors_command_led_power = param2;
             read_optical_sensors_command_flag = true;
             result = param1;
-            start_temperature_control();
             break;
         case 5: // read optical sensors (param) without moving to read position
             stop_temperature_control();
@@ -2186,7 +2184,9 @@ void loop()
 
         if (read_optical_sensors_command_flag) {
             read_optical_sensors_command_flag = false;
+            stop_temperature_control();
             read_optical_sensors(read_optical_sensors_command_param, read_optical_sensors_command_led_power, false);
+            start_temperature_control();
         }
     }
     
