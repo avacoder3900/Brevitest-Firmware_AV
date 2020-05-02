1e85d0ca18c7bdfccea1273588b363f265bcabec
Leo Linbeck III <leo3@linbeck.com>
2020-05-02 16:30:57 -0500

    Reorganize folder structure

--- firmware-v23/brevitest-firmware/target/1.5.0/argon/obj/src/user_module.o.d
+++
@@ -1,8 +0,0 @@
-/Users/leo3/github/brevitest-device/firmware-v23/brevitest-firmware/target/1.5.0/argon//obj/./src/user_module.o: \
- src/user_module.c \
- ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
- ../../../dynalib/inc/dynalib.h
-
-../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
-
-../../../dynalib/inc/dynalib.h:
