a30bc70221fc68a32246359fc11d0b22742fceee
Leo Linbeck III <leo3@linbeck.com>
2020-05-04 15:31:39 -0500

    Recompile v15

--- firmware/src/brevitest-firmware.cpp
+++ firmware/src/brevitest-firmware.cpp
@@ -1123,7 +1123,7 @@ void brevitest_publish(String event_name, char *uuid)
 
     callback_complete = false;
     callback_buffer[0] = '\0';
-    Particle.publish(String("brevitest-production"), event_name + String(ITEM_DELIM) + String(uuid), PRIVATE, NO_ACK);
+    Particle.publish(String("brevitest-development"), event_name + String(ITEM_DELIM) + String(uuid), PRIVATE, NO_ACK);
     Serial.printlnf("Publish: event = %s, uuid = %s", event_name.c_str(), uuid);
 }
 
@@ -1780,12 +1780,14 @@ int particle_command(String arg)
         case 25: // return stage position
             result = stage_position;
             break;
-        case 26: // magnetometer test param1 = step distance
+        case 26: // magnetometer test param1 = step distance, param2 = test interval
             reset_stage(false);
+            move_stage(-MICRONS_TO_INITIAL_POSITION, SLOW_STEP_DELAY);
             indx = get_next_command_param(arg, indx, &param1, MAGNETOMETER_TEST_DEFAULT_STEP_DISTANCE);
+            indx = get_next_command_param(arg, indx, &param2, MAGNETOMETER_TEST_INTERVAL);
             test_magnetometer_command_flag = true;
             test_magnetometer_command_microns_to_move = param1;
-            test_magnetometer_timer.start();
+            test_magnetometer_timer.changePeriod(param2);
             result = 1;
             break;
         case 27: // scan i2c bus
@@ -2042,8 +2044,8 @@ void configure_analyzer()
     Particle.function("run_test", particle_run_test);
 
     device_id = System.deviceID();
-    Particle.subscribe(String(device_id + "/hook-response/brevitest-production/"), brevitest_callback, MY_DEVICES);
-    Particle.subscribe(String(device_id + "/hook-error/brevitest-production/"), brevitest_error, MY_DEVICES);
+    Particle.subscribe(String(device_id + "/hook-response/brevitest-development/"), brevitest_callback, MY_DEVICES);
+    Particle.subscribe(String(device_id + "/hook-error/brevitest-development/"), brevitest_error, MY_DEVICES);
 
     init_digital_pin(pinStageLimit, INPUT_PULLUP, 0);
     init_digital_pin(pinCartridgeLoaded, INPUT_PULLUP, 0);
