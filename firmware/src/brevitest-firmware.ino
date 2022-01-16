006e878efc78e1a5da00b5008a3ac8a80e539c0e
Leo Linbeck III <leo3@linbeck.com>
2022-01-16 17:59:01 -0600

    Add line break in stress test optical output

--- firmware/src/brevitest-firmware.ino
+++ firmware/src/brevitest-firmware.ino
@@ -2034,8 +2034,8 @@ void do_stress_test_step(int step) {
             stress_test_oscillate_stage(-4500, 350, 100);
             break;
         case 9: // read baseline sensors
-            if (enable_optical_system(true))
-            {
+            if (enable_optical_system(true)) {
+                Serial.println();
                 set_baselines();
                 stress_test_read_optical_sensors(OPTICAL_SENSOR_DEFAULT_PARAM, stress_test_LED_power);
             }
@@ -2062,8 +2062,8 @@ void do_stress_test_step(int step) {
             stress_test_delay(300);
             break;
         case 14: // read sensors
-            if (enable_optical_system(true))
-            {
+            if (enable_optical_system(true)) {
+                Serial.println();
                 test.number_of_readings = 0;
                 stress_test_read_optical_sensors(OPTICAL_SENSOR_DEFAULT_PARAM, stress_test_LED_power);
                 stress_test_read_optical_sensors(OPTICAL_SENSOR_DEFAULT_PARAM, stress_test_LED_power);
